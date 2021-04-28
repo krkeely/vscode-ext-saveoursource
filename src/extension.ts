874f2c3157da18cfc8a643ce71bddb02bb23938a
krkeely <kathleenrkeely@gmail.com>
2021-04-28 19:49:16 +1000

    syntax correction

--- src/extension.ts
+++ src/extension.ts
@@ -5,18 +5,26 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "saveoursource" is now active!');
+	vscode
 	var unsavedFiles = vscode.workspace.textDocuments.map(file => {
 		if (file.isDirty)
-			return file.fileName
+			return file
 	} );
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('saveoursource.checkUnsaved', () => {
-		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('all unsaved files '+ unsavedFiles, );
+	let disposable = vscode.commands.registerCommand('saveoursource.checkUnsaved', () => {
+		let SaveAll = 'Save all'
+		vscode.window.showInformationMessage('all unsaved files '+ unsavedFiles.forEach(file => file?.fileName), SaveAll )
+		.then(selection => {
+			if(selection == SaveAll){
+				unsavedFiles.forEach(file => {
+					if(!file?.save()){
+						vscode.window.showInformationMessage('failed to save ' + file?.fileName)
+						console.log('failed to save ' + file?.fileName)
+					}
+				});
+			}
+		});
+		
 	});
 	context.subscriptions.push(disposable);
 }
