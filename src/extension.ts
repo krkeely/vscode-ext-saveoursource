46866f7d51938fa7ccbbbb1c5b44e67b322bbdd1
krkeely <kathleenrkeely@gmail.com>
2021-05-03 14:24:15 +1000

    trigger save all option on new terminal

--- src/extension.ts
+++ src/extension.ts
@@ -4,29 +4,38 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "saveoursource" is now active!');
-	vscode
-	var unsavedFiles = vscode.workspace.textDocuments.map(file => {
-		if (file.isDirty)
-			return file
-	} );
+	console.log('"saveoursource" is now active');
 
-	let disposable = vscode.commands.registerCommand('saveoursource.checkUnsaved', () => {
-		let SaveAll = 'Save all'
-		vscode.window.showInformationMessage('all unsaved files '+ unsavedFiles.forEach(file => file?.fileName), SaveAll )
-		.then(selection => {
-			if(selection == SaveAll){
-				unsavedFiles.forEach(file => {
-					if(!file?.save()){
-						vscode.window.showInformationMessage('failed to save ' + file?.fileName)
-						console.log('failed to save ' + file?.fileName)
-					}
-				});
+	function checkUnsavedFiles () {
+		console.log('checking for unsaved files');
+		let unsavedFiles = vscode.workspace.textDocuments.map(file => {
+			if (file.isDirty)
+				return file
+		} );
+
+		if(vscode.window.activeTerminal && unsavedFiles.length > 0){
+			let saveAll = 'Save All'
+			let message = 'Unsaved Files:'
+			for (let file of unsavedFiles){
+				message += '\r' + file?.fileName;
 			}
-		});
-		
-	});
-	context.subscriptions.push(disposable);
+			vscode.window.showInformationMessage(message, saveAll)
+			.then(selection => {
+				if(selection === saveAll){
+					unsavedFiles.forEach(file => {
+						if(!file?.save()){
+							vscode.window.showInformationMessage('failed to save ' + file?.fileName)
+							console.log('failed to save ' + file?.fileName)
+						}
+					});
+				}
+					
+			});
+		}
+
+	}
+	let changeListener = vscode.window.onDidChangeActiveTerminal(checkUnsavedFiles);		
+	context.subscriptions.push(changeListener);
 }
 
 // this method is called when your extension is deactivated
